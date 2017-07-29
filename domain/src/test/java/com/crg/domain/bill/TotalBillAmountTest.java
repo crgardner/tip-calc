4219017c9b41d207361ebf67383edba968d62a0f
cgardner <chris.r.gardner@gmail.com>
2017-07-29 18:51:44 -0400

    Add tests

---
+++ domain/src/test/java/com/crg/domain/bill/TotalBillAmountTest.java
@@ -0,0 +1,19 @@
+package com.crg.domain.bill;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.Test;
+
+import com.crg.domain.money.MoneyFactory;
+
+public class TotalBillAmountTest {
+    private TotalBillAmount totalBillAmount;
+
+    @Test
+    public void holdsTotalBillAmountAndTipAmount() throws Exception {
+        totalBillAmount = new TotalBillAmount(MoneyFactory.usdOf(1.50),MoneyFactory.usdOf(45.50));
+        
+        assertThat(totalBillAmount.tipAmount()).isEqualTo(MoneyFactory.usdOf(1.50));
+        assertThat(totalBillAmount.totalAmount()).isEqualTo(MoneyFactory.usdOf(45.50));
+    }
+}
