2f15c6f8bc66bab80797065d264f153c73168a25
cgardner <chris.r.gardner@gmail.com>
2017-12-22 18:01:24 -0500

    Add test-support and complete junit jupiter support.

--- api/src/test/java/com/crg/api/hello/AnotherHelloWorldTest.java
+++ api/src/test/java/com/crg/api/hello/AnotherHelloWorldTest.java
@@ -1,12 +1,14 @@
 package com.crg.api.hello;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
 public class AnotherHelloWorldTest {
 
     @Test
     public void test() {
-        
+        assertThat(false).isFalse();
     }
 
 }
